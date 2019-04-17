07fec963e416722f7d3573588a8f73b0aedb1307
Aya <78706155@qq.com>
2019-04-17 23:11:59 +0800

    add rank in comment

--- front_end/js/addRatePage.js
+++ front_end/js/addRatePage.js
@@ -3,6 +3,18 @@ function selectTeacher(name){
   $("#buttonSelectTeacher").html(name); 
 }
 
+function raty(number,id){
+  $(id).raty({
+    score:number,
+    starOn:"./resource/star-on.png",
+    starOff:"./resource/star-off.png",
+    starHalf:"./resource/star-half.png",
+    readOnly:false,
+    halfShow:true,
+    size:34,
+ });
+}
+
 $(document).ready(function() {
 
   if($.cookie("username") != undefined){
@@ -14,8 +26,8 @@ $(document).ready(function() {
   //1 加载课程名称 学院
   var coursenum=parseInt(window.sessionStorage.getItem("coursetoload"));
   $("#course_name").html(window.sessionStorage.getItem("course"+coursenum+"name"));
-  var teacher_list=window.sessionStorage.getItem("course"+coursenum+"teacher_list").split(',');
-  
+  //var teacher_list=window.sessionStorage.getItem("course"+coursenum+"teacher_list").split(',');
+  var teacher_list=["教师1"];
   var data="";
   for(var i=0; i<teacher_list.length;i++){
     data+="<li>\n"+
@@ -27,6 +39,13 @@ $(document).ready(function() {
     }
     $("#teacherlist").html(data);
   }
+
+  raty(5,"#difficulty_score");
+  raty(5,"#funny_score");
+  raty(5,"#gain_score");
+  raty(5,"#recommend_score");
+
+  console.log($("#difficulty_score").raty("getScore"));
 
 })
 
@@ -64,6 +83,40 @@ function Func_submit() {
         'course_ID': window.sessionStorage.getItem("course"+coursenum+"course_ID"),
         'content' : $("#comment").val(),
         'teacher_name' : $("#buttonSelectTeacher").text()
+      },
+      xhrFields: {
+        withCredentials: true
+      },
+      success:function(data){
+        //	alert("ajax success");
+        //console.log(data);
+        //console.log(data.status)
+        if(data.status=="1"){
+            //alert(data.body.message);
+            //console.log("Successfully makeComment "+coursenum);
+            //alert("评论成功！");
+            //window.setTimeout("location.href='./coursePage.html'", 1000);
+        }
+        else{
+            alert(data.errMsg);
+        }  
+      },
+      error:function(data){
+          alert(JSON.stringify(data));
+      }
+    });
+
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: "https://api.ratemycourse.tk/makeRank/",
+      data: {
+        'username': $.cookie("username"),
+        'course_ID': window.sessionStorage.getItem("course"+coursenum+"course_ID"),
+        'difficulty_score': $("#difficulty_score").raty("getScore"),
+        'funny_score': $("#funny_score").raty("getScore"),
+        'gain_score': $("#gain_score").raty("getScore"),
+        'recommend_score': $("#recommend_score").raty("getScore")
       },
       xhrFields: {
         withCredentials: true
