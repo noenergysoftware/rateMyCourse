8a48705e3044639dad7e2d74d7f93dddaf8f9caa
supplient <437566830@qq.com>
2019-04-20 08:20:09 +0800

    Add finally for LogStatus to avoid all testcases failed.

--- test/user_actions.py
+++ test/user_actions.py
@@ -3,8 +3,10 @@ from contextlib import contextmanager
 
 @contextmanager
 def LogStatus(page, name, password):
-    driver = page.driver
-    if not page is LoginPage:
-        page = page.goLoginPage()
-    yield page.logIn(name, password)
-    driver.delete_all_cookies()
+    try:
+        driver = page.driver
+        if not page is LoginPage:
+            page = page.goLoginPage()
+        yield page.logIn(name, password)
+    finally:
+        driver.delete_all_cookies()
