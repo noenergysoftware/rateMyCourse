197efe2afbb37c3f5a8d866480626704eb61cf04
sfdairong0204@yeah.net <sfdairong0204@yeah.net>
2019-04-24 14:08:01 +0800

    first commit

--- rateMyCourse/views/logs.py
+++
@@ -1,9 +0,0 @@
-import os
-def writeLog(textin):
-    try:
-        with open("log.txt","a") as fout:
-            fout.write(textin+"\n")
-    except:
-        return 1
-    else:
-        return 0
