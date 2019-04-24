84c1fd1ba5071cea2ef79c70ceec5cfe0052cd91
Aya <78706155@qq.com>
2019-04-24 20:58:26 +0800

    move third party js to lib

---
+++ front_end/js/login.js
@@ -0,0 +1,60 @@
+﻿$(document).ready(function () {
+        function login() {
+            var name;
+            var email;
+            if ($("#name").val().indexOf("@") == -1) {       //true:是name登录
+                name = document.getElementById("name").value;
+                
+            } else {
+                
+                email = document.getElementById("name").value;
+            }
+            
+            //document.getElementByIssd("login").disabled = true;
+            $.ajax({
+                async: false,
+                type: "POST",
+                url: "https://api.ratemycourse.tk/signIn/",
+                dataType: "json",
+                data: {
+                    username: name,
+                    mail: email,
+                    password: md5($("#password").val())
+                },
+                xhrFields: {
+                    withCredentials: true
+                },
+                success: function (data) {
+                    //data=JSON.parse(data);
+                    console.log(JSON.stringify(data));
+                    console.log(data.status);
+                    if (data.status == "1") {
+                        $.cookie("username", data.body.username, { path: '/' });
+                        //window.sessionStorage.setItem("status",data.status);           //登录成功"status"="0",用于切换导航栏
+                        //window.sessionStorage.setItem("username",data.body.username);
+                        //console.log("status"+window.sessionStorage.getItem("status"));
+                        alert(data.body.username + "登录成功！");
+                        window.setTimeout("location.href='./index.html'", 1200);
+                    }
+                    else {
+                        console.log(JSON.stringify(data));
+                        alert(data.errMsg);
+                    }
+                    //window.setTimeout("location.href='/user'", 1200);
+                },
+                error: function (data) {
+                    console.log(JSON.stringify(data));
+                    alert(JSON.stringify(data));
+                }
+            });
+        }
+        $("html").keydown(function (event) {
+            //13 = enter
+            if (event.keyCode == 13) {
+                login();
+            }
+        });
+        $("#login").click(function () {
+            login();
+        });
+    })
---
+++ front_end/js/signup.js
@@ -0,0 +1,123 @@
+﻿$(document).ready(function(){
+				var Captcha=false;
+				var Res;
+				window.callback = function(res){
+						//console.log(res)
+						// res（用户主动关闭验证码）= {ret: 2, ticket: null}
+						// res（验证成功） = {ret: 0, ticket: "String", randstr: "String"}
+						if(res.ret === 0){
+								//alert(res.ticket)   // 票据
+								Captcha=true;
+								$("#TencentCaptcha").text("人机验证通过");
+								$("#TencentCaptcha").attr("disabled","disabled"); 
+								Res=res;
+						}
+				}
+				var IP=returnCitySN["cip"];
+				console.log(IP);
+				
+
+				function register(){
+						console.log(Res.ticket);
+						console.log(Res.randstr);
+						$.ajax({
+								async: false,
+								type:"POST",
+								url: "https://api.ratemycourse.tk/signUp/",
+								dataType:"json",
+								data:{              
+										username: $("#name").val(),
+										mail: $("#email").val(),
+										password: md5($("#passwd").val()),
+										IP:IP,
+										Ticket: Res.ticket,
+										Randstr: Res.randstr
+								},
+								xhrFields: {
+										withCredentials: true
+								},
+								success:function(data){
+										//data=JSON.parse(data);
+									//	alert("ajax success");
+										console.log(data);
+										console.log(data.status)
+										if(data.status=="1"){
+												alert(data.body.message);
+												window.setTimeout("location.href='./login.html'", 1000);
+										}
+										else{
+											alert(data.errMsg);
+										}
+										
+								},
+								error:function(data){
+									alert(JSON.stringify(data));
+								}
+						});
+				}
+				$("#tos").click(function(){
+						if(check(Captcha)==true){
+							register();
+						}
+        });
+				
+				$("html").keydown(function (event) {
+            //13 = enter
+            if (event.keyCode == 13) {
+								if(check(Captcha)==true){
+										register();
+								}
+            }
+        });
+
+    });
+function check(check_Captcha){
+		var errmsg="";
+
+		var name=$("#name").val();
+		console.log(name);
+		var uPattern = /^[a-zA-Z0-9_]{3,16}$/;
+		var check_name=uPattern.test(name);
+		console.log(check_name);
+
+		if(check_name==false){
+				errmsg=errmsg+"用户名格式错误\n";
+		}
+
+		var email=$("#email").val();
+		console.log(email);
+		var ePattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+		var check_email=ePattern.test(email)
+		console.log(check_email);
+
+		if(check_email==false){
+				errmsg=errmsg+"邮箱格式错误\n";
+		}
+		
+		var password=$("#passwd").val();
+		var repassword=$("#repasswd").val();
+		console.log(password);
+		console.log(repassword);
+		var pdPattern = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]).{8,30}$/;
+		var check_password=pdPattern.test(password);
+		console.log(check_password);
+
+		if(check_password==false){
+			errmsg=errmsg+"密码格式错误\n";
+		}
+		
+		if(password!=repassword){
+			errmsg=errmsg+"两次密码不一致\n";
+			check_password=false;
+		}
+
+		if(check_Captcha==false){
+			errmsg=errmsg+"人机验证未通过\n";
+		}
+
+		if( check_name==false || check_email==false || check_password==false || check_Captcha==false){
+			alert(errmsg);
+			return false;
+		}
+		return true;
+	}
