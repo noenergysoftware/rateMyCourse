f9ef641eec2affc273610951047e84703dff9311
supplient <437566830@qq.com>
2019-04-28 13:55:40 +0800

    Add proxy and remove ssl verify for test_manage.

--- test_manage.py
+++ test_manage.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 import os
 import sys
+import urllib.request
+import ssl
 
 if __name__ == "__main__":
+    # Set proxy which leads to Fiddler
+    # and Fiddler will forward urllib's requests.
+    proxy = urllib.request.ProxyHandler({"https": "https://127.0.0.1:8888"})
+    opener = urllib.request.build_opener(proxy)
+    urllib.request.install_opener(opener)
+
+    # Remove SSL verify, since we are just testing.
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    # django
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flamenco.test_settings")
     try:
         from django.core.management import execute_from_command_line
