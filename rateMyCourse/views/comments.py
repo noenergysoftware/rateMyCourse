360f55e7d5d1898ef34a1c52be16af97599161eb
canuse <liu_jc@buaa.edu.cn>
2019-05-07 16:32:54 +0800

    disable some cache

--- rateMyCourse/views/comments.py
+++ rateMyCourse/views/comments.py
@@ -306,7 +306,7 @@ def get_rate_comment(request):
         }), content_type="application/json")
 
 
-@cache_page(60 * 60 * 2)
+#@cache_page(60 * 60 * 2)
 def get_high_rate_comment(request):
     try:
         course_ID = request.GET['course_ID']
