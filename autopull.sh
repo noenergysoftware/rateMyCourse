035ebb49252df53e9c326527bf60529d0c486444
canuse <liu_jc@buaa.edu.cn>
2019-06-03 20:43:14 +0800

    remove some files

--- autopull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /home/canuse/mctest/
-git pull
-cd ..
-python3 /home/canuse/mctest/manage.py makemigrations
-python3 /home/canuse/mctest/manage.py migrate
-python3 /home/canuse/mctest/manage.py runserver 17171
-
-
